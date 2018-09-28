a323cf521331f136dd698feea20d08a4e0aeae45
Rchave <rchave2801@hotmail.com>
2018-09-27 20:45:29 -0500

    Added properties file

--- src/main/java/com/facturacion/controller/DBConnection.java
+++ src/main/java/com/facturacion/controller/DBConnection.java
@@ -1,16 +1,30 @@
 package com.facturacion.controller;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.Properties;
 
 
 public class DBConnection {
 	private static Connection connect = null;
+	private static Properties props = new Properties();
+	private static InputStream input = null;
 	
 	public static Connection createConnection(){
-		String url = "jdbc:mysql://localhost:3308/BillingDB?useSSL=false";
-		String username = "billingApp";
-		String password = "billing_App";
+		try {
+		input = new FileInputStream("application.properties");
+		props.load(input);
+		} catch(IOException e) {
+			e.printStackTrace();
+		}	
+		
+		String url = props.getProperty("url");
+		String username = props.getProperty("username");
+		String password = props.getProperty("password");
+		
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			connect = DriverManager.getConnection(url, username, password);
